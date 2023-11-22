9ea6e617e93c7e218631bf06b93066d67c60b2bb
Heiya <morooka.akira.inon29@gmail.com>
2023-11-22 16:50:03 +0900

    add page

---
+++ src/pages/api/ip.ts
@@ -0,0 +1,12 @@
+export default function handler(req, res) {
+  // リクエストからIPアドレスを取得
+  let ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  
+  // ローカルホストのIPアドレスを適切に処理
+  if (ip === '::1' || ip.startsWith('::ffff:')) {
+    ip = '127.0.0.1';
+  }
+
+  // IPアドレスをJSON形式で返す
+  res.status(200).json({ ip: ip });
+}
